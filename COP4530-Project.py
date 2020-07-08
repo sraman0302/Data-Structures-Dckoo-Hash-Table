f02deba7603604f660eebe21bb3668e37f9153d6
Siddhant Raman <sraman0302@gmail.com>
2020-07-07 22:54:31 -0400

    Class Template

--- COP4530-Project.py
+++ COP4530-Project.py
@@ -1,3 +1,35 @@
+class Dynamic_linear_hash_table:
+    def __init__(self):
+        self.name = None
+
+    def size(self):
+        print()
+
+    def empty():
+        print()
+
+    def capacity():
+        print()
+
+    def member():
+        print()
+
+    def load_factor():
+        print()
+
+    def bin():
+        print()
+
+    def insert():
+        print()
+
+    def remove():
+        print()
+
+    def clear():
+        print()
+
+
 def main():
     print()
 
