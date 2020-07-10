026acfcd26910de135c8a1942a2e34d1e7a5ebe6
Siddhant Raman <sraman0302@gmail.com>
2020-07-10 02:22:45 -0400

    class template

--- COP4530-Project.py
+++ COP4530-Project.py
@@ -1,32 +1,43 @@
 class Dynamic_linear_hash_table:
-    def __init__(self):
+    def __init__(self, count, currcapp, icapp):
         self.name = None
+        self.count = count
+        self.currCapacity = currcapp
+        self.initCapacity = icapp
+
+# Accessors
 
     def size(self):
+        return self.count
+
+    def empty(self):
+        if (self.count == 0):
+            return False
+        else:
+            return True
+
+    def capacity(self, array):
+        return (self.count-array.count(None))
+
+    def member(self, val, array):
+        if val in array:
+            return True
+        return False
+
+    def load_factor(self, array):
         print()
 
-    def empty():
+    def bin(self, array):
+        print()
+# Mutators
+
+    def insert(self):
         print()
 
-    def capacity():
+    def remove(self):
         print()
 
-    def member():
-        print()
-
-    def load_factor():
-        print()
-
-    def bin():
-        print()
-
-    def insert():
-        print()
-
-    def remove():
-        print()
-
-    def clear():
+    def clear(self):
         print()
 
 
